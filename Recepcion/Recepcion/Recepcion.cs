42127c0e2bc63e3e65aa0dad1e2fa65f7d55da7c
Conny <cbarriosm1@miumg.edu.gt>
2020-02-17 10:00:21 -0600

    V10

--- Recepcion/Recepcion/Recepcion.cs
+++ Recepcion/Recepcion/Recepcion.cs
@@ -21,6 +21,7 @@ namespace Recepcion
         public string cui;
         public string tramite;
         public string TipoTramite = " ";
+        DateTime fe = new DateTime();
         SQL_Bitacora log = new SQL_Bitacora();
 
         public void ObtenerCui(string cui)
@@ -55,44 +56,10 @@ namespace Recepcion
             {
 
             }
-            /*
-             switch (TipoTramite )
-            {
-                case "Menor de edad":
-                    Tbc_Recepcion.SelectedIndex = 0;
-                    break;
-                case "Mayor de edad":
-                    Tbc_Recepcion.SelectedIndex = 1;
-                    break;
-                case "Adulto Mayor de 60 años":
-                    Tbc_Recepcion.SelectedIndex = 2;
-                    break;
-            }
-             */
         }
 
         private void Btn_DPIMayoresACopia_Click(object sender, EventArgs e)
         {
-            /*
-               Ofd_MayoresA.InitialDirectory = "C:\\Documents";
-               Ofd_MayoresA.Filter = "Archivo PDF (*.PDF)|*.PDF";
-
-               if (Ofd_MayoresA.ShowDialog() == DialogResult.OK)
-               {
-                   try
-                   {
-                       this.fileUpload = Ofd_MayoresA.FileName.ToString();
-                       nombre = this.fileUpload.Split('\\');
-                       string sRutaMayoresA = Ofd_MayoresA.FileName;
-                       Txt_DPICopiaMayoresA.Text = sRutaMayoresA;
-                   }
-                   catch (Exception)
-                   {
-                       throw;
-                   }
-               }
-            */
-
             String[] nombre;
             String fileUpload;
 
@@ -121,24 +88,6 @@ namespace Recepcion
 
         private void Btn_DPIMayoresCopia_Click(object sender, EventArgs e)
         {
-            /*
-            Ofd_Mayores.InitialDirectory = "C:\\Documents";
-            Ofd_Mayores.Filter = "Archivo PDF (*.PDF)|*.PDF";
-
-            if (Ofd_Mayores.ShowDialog() == DialogResult.OK)
-            {
-                try
-                {
-                    string sRutaDPIMayores = Ofd_Mayores.FileName;
-                    Txt_DPICopiaMayores.Text = sRutaDPIMayores;
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-            }
-            */
-
             String[] nombre;
             String fileUpload;
 
@@ -157,24 +106,6 @@ namespace Recepcion
 
         private void Btn_BoletoOrnatoCopiaMayores_Click(object sender, EventArgs e)
         {
-            /*
-            Ofd_Mayores.InitialDirectory = "C:\\Documents";
-            Ofd_Mayores.Filter = "Archivo PDF (*.PDF)|*.PDF";
-
-            if (Ofd_MayoresA.ShowDialog() == DialogResult.OK)
-            {
-                try
-                {
-                    string sRutaBoletoMayores = Ofd_Mayores.FileName;
-                    Txt_DPICopiaMayores.Text = sRutaBoletoMayores;
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-            }
-            */
-
             String[] nombre;
             String fileUpload;
 
@@ -208,24 +139,6 @@ namespace Recepcion
 
         private void Btn_DPICopiaPadre_Click(object sender, EventArgs e)
         {
-            /*
-            Ofd_Menores.InitialDirectory = "C:\\Documents";
-            Ofd_Menores.Filter = "Archivo PDF (*.PDF)|*.PDF";
-
-            if (Ofd_Menores.ShowDialog() == DialogResult.OK)
-            {
-                try
-                {
-                    string sRutaDPIPadre = Ofd_Menores.FileName;
-                    Txt_DPIPadreCopia.Text = sRutaDPIPadre;
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-            }
-            */
-
             String[] nombre;
             String fileUpload;
 
@@ -244,24 +157,6 @@ namespace Recepcion
 
         private void Btn_DPICopiaMadre_Click(object sender, EventArgs e)
         {
-            /*
-            Ofd_Menores.InitialDirectory = "C:\\Documents";
-            Ofd_Menores.Filter = "Archivo PDF (*.PDF)|*.PDF";
-
-            if (Ofd_Menores.ShowDialog() == DialogResult.OK)
-            {
-                try
-                {
-                    string sRutaDPIMadre = Ofd_Menores.FileName;
-                    Txt_DPIMadreCopia.Text = sRutaDPIMadre;
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-            }
-            */
-
             String[] nombre;
             String fileUpload;
 
@@ -280,24 +175,6 @@ namespace Recepcion
 
         private void Btn_CertiNaci_Click(object sender, EventArgs e)
         {
-            /*
-            Ofd_Menores.InitialDirectory = "C:\\Documents";
-            Ofd_Menores.Filter = "Archivo PDF (*.PDF)|*.PDF";
-
-            if (Ofd_Menores.ShowDialog() == DialogResult.OK)
-            {
-                try
-                {
-                    string sRutaCertificado = Ofd_Menores.FileName;
-                    Txt_CertiNaci.Text = sRutaCertificado;
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-            }
-            */
-
             String[] nombre;
             String fileUpload;
 
@@ -316,24 +193,6 @@ namespace Recepcion
 
         private void Btn_CartaPoder_Click(object sender, EventArgs e)
         {
-            /*
-            Ofd_Menores.InitialDirectory = "C:\\Documents";
-            Ofd_Menores.Filter = "Archivo PDF (*.PDF)|*.PDF";
-
-            if (Ofd_Menores.ShowDialog() == DialogResult.OK)
-            {
-                try
-                {
-                    string sRutaCartaPoder = Ofd_Menores.FileName;
-                    Txt_CartaPoder.Text = sRutaCartaPoder;
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-            }
-            */
-
             String[] nombre;
             String fileUpload;
 
@@ -352,24 +211,6 @@ namespace Recepcion
 
         private void Btn_BoletaMenor_Click(object sender, EventArgs e)
         {
-            /*
-            Ofd_Menores.InitialDirectory = "C:\\Documents";
-            Ofd_Menores.Filter = "Archivo PDF (*.PDF)|*.PDF";
-
-            if (Ofd_Menores.ShowDialog() == DialogResult.OK)
-            {
-                try
-                {
-                    string sRutaBoletoMenor = Ofd_Menores.FileName;
-                    Txt_BoletoOrnatoCopiaMenor.Text = sRutaBoletoMenor;
-                }
-                catch (Exception)
-                {
-                    throw;
-                }
-            }
-            */
-
             String[] nombre;
             String fileUpload;
 
@@ -412,6 +253,8 @@ namespace Recepcion
             users.Ingresar_Menores(tramite, cui, Txt_DPIMadre.Text, Txt_DPIPadre.Text, Txt_DPIPadreCopia.Text, Txt_DPIMadreCopia.Text,
                 Txt_CertiNaci.Text, Txt_CartaPoder.Text, Txt_BoletoOrnatoMenor.Text, Txt_BoletoOrnatoCopiaMenor.Text, Txt_BoletaMenor.Text);
             this.Hide();
+
+            log.Ingresar_Bitacora(cui, "Documentos enviados del Menor de Edad", fe.ToString());
         }
 
         private void Btn_EnviarMayoresA_Click(object sender, EventArgs e)
@@ -419,13 +262,18 @@ namespace Recepcion
             SQL_MayoresA users = new SQL_MayoresA();
             users.Ingresar_MayoresA(tramite, cui, Txt_DPICopiaMayoresA.Text, Txt_BoletaMayoresA.Text);
             this.Hide();
+
+            log.Ingresar_Bitacora(cui, "Documentos enviados del Meyor de Edad", fe.ToString());
         }
 
         private void Btn_EnviarMayores_Click(object sender, EventArgs e)
         {
             SQL_Mayores users = new SQL_Mayores();
-            users.Ingresar_Mayores(tramite, cui, Txt_DPICopiaMayores.Text, Txt_BoletoOrnatoMayores.Text, Txt_BoletoOrnatoCopiaMayores.Text, Txt_BoletaMayores.Text);
+            users.Ingresar_Mayores(tramite, cui, Txt_DPICopiaMayores.Text, Txt_BoletoOrnatoMayores.Text,
+                Txt_BoletoOrnatoCopiaMayores.Text, Txt_BoletaMayores.Text);
             this.Hide();
+
+            log.Ingresar_Bitacora(cui, "Documentos enviados del Meyor de 60 Años", fe.ToString());
         }
 
         private void HideAllTabsOnTabControl(TabControl theTabControl)
